b3d994c11636e6f1cb05ffbe790521ff1258f63b
Bracha Horvitz <brachahorvitz@gmail.com>
2026-04-13 20:48:10 +0300

    feat: initial project setup

---
+++ src/main/java/com/finpipeline/financial_pipeline/FinancialPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.finpipeline.financial_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finpipeline/financial_pipeline/FinancialPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpipeline.financial_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
